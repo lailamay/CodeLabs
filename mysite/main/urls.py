ccae39c44fdd68a80cb1d993ea9ba417a6cb08db
Laila Chavez <lailamay.chavez@gmail.com>
2020-07-21 23:34:36 -0700

    added base and home files to templates

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path('<int:scenariolist_id>/student/', views.student, name='student page'),
-    path('<int:scenariolist_id>/', views.scenario, name='scenarios')
+    path("<int:scenariolist_id>/", views.index, name='index'),
+    path("", views.home, name='home'),
+    #path('<int:scenariolist_id>/student/', views.student, name='student page'),
+    #path('<int:scenariolist_id>/', views.scenario, name='scenarios')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,22 +5,26 @@ from .models import ScenarioList, Scenario
 
 #This is where we put what we want to show on our app
 
-def index(response):
-    scenario_list = ScenarioList.objects.all()
-    #scenario_list = ScenarioList.objects.get(id=id)
-    context = {'scenario_list': scenario_list}
-    return render(response, 'main/index.html', context)
-    #return HttpResponse("<h1>%s</h1>" %scenario_list.name)
+def index(response, id):
+    #scenario_list = ScenarioList.objects.all()
+    scenario_list = ScenarioList.objects.get(id=id)
+    #context = {'scenario_list': scenario_list}
+    #return render(response, 'main/index.html', context)
+    return render(response, "main/scenario.html", {"scenario_list":scenario_list})
 
 
-def student(response, scenariolist_id):
-    try:
-        scenario_list = ScenarioList.objects.get(pk=scenariolist_id)
-    except ScenarioList.DoesNotExist:
-        raise Http404("Scenario does not exist")
-    return render(response, 'main/student.html', {'scenario_list': scenario_list})
+# def student(response, scenariolist_id):
+#     try:
+#         scenario_list = ScenarioList.objects.get(pk=scenariolist_id)
+#     except ScenarioList.DoesNotExist:
+#         raise Http404("Scenario does not exist")
+#     return render(response, 'main/student.html', {'scenario_list': scenario_list})
 
 
-def scenario(response, scenariolist_id):
-    scenario_list = get_object_or_404(ScenarioList, pk= scenariolist_id)
-    return render(response, 'main/scenario.html',  {'scenario_list': scenario_list})
+def home(response):
+	return render(response, "main/home.html", {})
+
+
+# def scenario(response, scenariolist_id):
+#     scenario_list = get_object_or_404(ScenarioList, pk= scenariolist_id)
+#     return render(response, 'main/scenario.html',  {'scenario_list': scenario_list})
