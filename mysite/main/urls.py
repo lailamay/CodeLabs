7c184a728911331de863bb81ce51da60eda84443
Laila Chavez <lailamay.chavez@gmail.com>
2020-07-22 14:51:23 -0700

    modified view.py and fixed id bug

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:scenariolist_id>/", views.index, name='index'),
+    path("<int:id>/", views.index, name='index'),
     path("", views.home, name='home'),
     #path('<int:scenariolist_id>/student/', views.student, name='student page'),
     #path('<int:scenariolist_id>/', views.scenario, name='scenarios')
