14459994cec0f267669d9440b503510354d44dc9
Laila Chavez <lailamay.chavez@gmail.com>
2020-07-23 00:30:54 -0700

    added custom form for creating new list and scenarios, sidebar and css styling

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("<int:id>/", views.index, name='index'),
     path("", views.home, name='home'),
+    path("create/", views.create, name="create"),
     #path('<int:scenariolist_id>/student/', views.student, name='student page'),
     #path('<int:scenariolist_id>/', views.scenario, name='scenarios')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,16 +1,60 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import ScenarioList, Scenario
+from .forms import CreateNewList
 
 #This is where we put what we want to show on our app
 
 def index(response, id):
     #scenario_list = ScenarioList.objects.all()
     scenario_list = ScenarioList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for scenario in scenario_list.scenario_set.all():
+                if response.POST.get("c" + str(scenario.id)) == "clicked":
+                    scenario.complete = True
+                else:
+                    scenario.complete = False
+
+                scenario.save()
+
+        elif response.POST.get("newScenario"):
+            txt = response.POST.get("newScenario")
+
+            if len(txt) > 2:
+                scenario_list.scenario_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
     #context = {'scenario_list': scenario_list}
     #return render(response, 'main/index.html', context)
     return render(response, "main/scenario.html", {"scenario_list":scenario_list})
+
+
+def home(response):
+	return render(response, "main/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            s = ScenarioList(name=n)
+            s.save()
+
+        return HttpResponseRedirect("/%i" %s.id)
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
+
+
+# def scenario(response, scenariolist_id):
+#     scenario_list = get_object_or_404(ScenarioList, pk= scenariolist_id)
+#     return render(response, 'main/scenario.html',  {'scenario_list': scenario_list})
 
 
 # def student(response, scenariolist_id):
@@ -19,12 +63,3 @@ def index(response, id):
 #     except ScenarioList.DoesNotExist:
 #         raise Http404("Scenario does not exist")
 #     return render(response, 'main/student.html', {'scenario_list': scenario_list})
-
-
-def home(response):
-	return render(response, "main/home.html", {})
-
-
-# def scenario(response, scenariolist_id):
-#     scenario_list = get_object_or_404(ScenarioList, pk= scenariolist_id)
-#     return render(response, 'main/scenario.html',  {'scenario_list': scenario_list})
