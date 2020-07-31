686bf181c3a4734a8cc068232c12e7b5fef2208c
Kate <kateliangg@gmail.com>
2020-07-31 00:40:37 -0500

    updated back-end

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import ScenarioList, Scenario, User, Student
-from django.contrib.admin.models import LogEntry
+#from django.contrib.auth.admin import UserAdmin
+
 
 
 # Register your models here.
@@ -9,27 +10,27 @@ admin.site.register(Scenario)
 admin.site.register(User)
 admin.site.register(Student)
 
-@admin.register(LogEntry)
-class LogEntryAdmin(admin.ModelAdmin):
-    # to have a date-based drilldown navigation in the admin page
-    date_hierarchy = 'action_time'
+# @admin.register(LogEntry)
+# class LogEntryAdmin(admin.ModelAdmin):
+#     # to have a date-based drilldown navigation in the admin page
+#     date_hierarchy = 'action_time'
 
-    # to filter the resultes by users, content types and action flags
-    list_filter = [
-        'user',
-        'content_type',
-        'action_flag'
-    ]
+#     # to filter the resultes by users, content types and action flags
+#     list_filter = [
+#         'user',
+#         'content_type',
+#         'action_flag'
+#     ]
 
-    # when searching the user will be able to search in both object_repr and change_message
-    search_fields = [
-        'object_repr',
-        'change_message'
-    ]
+#     # when searching the user will be able to search in both object_repr and change_message
+#     search_fields = [
+#         'object_repr',
+#         'change_message'
+#     ]
 
-    list_display = [
-        'action_time',
-        'user',
-        'content_type',
-        'action_flag',
-    ]
+#     list_display = [
+#         'action_time',
+#         'user',
+#         'content_type',
+#         'action_flag',
+#     ]
